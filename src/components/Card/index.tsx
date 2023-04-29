9a5965ed01beeb6f398ad6cf505c52c39c641e56
Gabriel Campos <gabrielcampoos@hotmail.com>
2023-04-28 23:50:04 -0300

    Game Feito

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,27 @@
+import './styles.css';
+
+export interface CardProps {
+	id: string;
+	flipped?: boolean;
+	back: string;
+	handleClick?: (id: string) => void;
+}
+
+export function Card({ flipped = false, back, handleClick, id }: CardProps) {
+	const cardContentClassNames = ['cardContent'];
+	flipped && cardContentClassNames.push('cardContentFlipped');
+	const handleClickFn = (id: string) => {
+		if (handleClick) {
+			handleClick(id);
+		}
+	};
+
+	return (
+		<div className="card" onClick={() => handleClickFn(id)}>
+			<div className={cardContentClassNames.join(' ')}>
+				<div className="cardFace cardFaceFront">?</div>
+				<div className="cardFace cardFaceBack">{back}</div>
+			</div>
+		</div>
+	);
+}
